df1d4bb43b2d7ccc84bdf7605e64fa2310996d96
repojohnray <repojohnray@users.noreply.github.com>
2016-09-13 21:32:07 +0200

    Update: autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
-
-aclocal -I m4 -I /usr/share/aclocal -I /usr/X11R6/share/aclocal -I /usr/local/share/aclocal --force;autoheader -f;libtoolize --copy --force;automake --add-missing --copy --foreign;autoconf --force;
+aclocal -I . -I m4 --force
+autoheader -f
+libtoolize --copy --force
+automake --add-missing --copy --foreign
+autoconf --force
