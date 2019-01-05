f60359ded52348900d2ff22d51a07ecb6a6be322
repojohnray <repojohnray@users.noreply.github.com>
2019-01-05 15:11:02 +0100

    Update: retro_get_memory_data() and retro_get_memory_size()

--- libretro/libretro.c
+++ libretro/libretro.c
@@ -20,6 +20,7 @@
 #include "fs/emu/path.h"
 #include "fs-uae/paths.h"
 #include "sources/src/include/uae_types.h"
+#include "sources/src/include/uae_memory.h"
 
 cothread_t mainThread;
 cothread_t emuThread;
@@ -44,8 +45,6 @@ static int firstps = 0;
 static enum retro_pixel_format fmt = RETRO_PIXEL_FORMAT_RGB565;
 
 
-extern uae_u8 *natmem_offset;
-extern uae_u32 natmem_size;
 extern unsigned short int  bmp[EMULATOR_MAX_WIDTH*EMULATOR_MAX_HEIGHT];
 extern unsigned short int  savebmp[EMULATOR_MAX_WIDTH*EMULATOR_MAX_HEIGHT];
 extern int pauseg;
@@ -798,14 +797,14 @@ bool retro_unserialize(const void *data_, size_t size)
 void *retro_get_memory_data(unsigned id)
 {
    if ( id == RETRO_MEMORY_SYSTEM_RAM )
-      return natmem_offset;
+      return fastmem_bank.baseaddr;
    return NULL;
 }
 
 size_t retro_get_memory_size(unsigned id)
 {
    if ( id == RETRO_MEMORY_SYSTEM_RAM )
-      return natmem_size;
+      return fastmem_bank.allocated;
    return 0;
 }
 
